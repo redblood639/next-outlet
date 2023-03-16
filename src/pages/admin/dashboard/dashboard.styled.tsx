b2fefbea35e0697c6482ab94f6c244724e6f4b95
redblood639 <albinorabbithunter@gmail.com>
2023-03-16 14:52:49 +0800

    feat : dashboard menu

--- src/pages/admin/dashboard/dashboard.styled.tsx
+++ src/pages/admin/dashboard/dashboard.styled.tsx
@@ -10,11 +10,38 @@ export const MenuWrapper = styled.div`
   height: 100%;
   background: #11192a;
   box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  gap: 15px;
+
+  box-sizing: border-box;
+  padding: 30px 0;
+`;
+
+export const MenuItem = styled.div`
+  width: 379px;
+  height: 71px;
+
+  background: #1f2737;
+  border-radius: 5px;
+
+  cursor: pointer;
+  box-sizing: border-box;
+  padding: 0 0 0 50px;
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background: #2e384b;
+  }
 `;
 
 export const DetailWrapper = styled.div`
   flex: 1;
-
   display: flex;
   justify-content: center;
   align-items: center;
